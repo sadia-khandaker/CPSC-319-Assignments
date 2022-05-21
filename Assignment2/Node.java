0a2a5272de33ed4e41966619a7ac0e06a1b5e5e0
sadiatsn <92899511+sadiatsn@users.noreply.github.com>
2022-05-21 16:43:17 -0600

    Add Node.java

---
+++ Assignment2/Node.java
@@ -0,0 +1,18 @@
+/**
+ * The Node Class
+ * Code adapted from Dr. Manzara's lecture note on D2L:
+ *
+ */
+
+
+public class Node {
+    public String data;
+    public Node next;
+
+    public Node( String data, Node next){
+        this.data = data;
+        this.next = next;
+    }
+
+}
+
